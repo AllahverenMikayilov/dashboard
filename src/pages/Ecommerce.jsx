5f69f0266c405d590e3f4df493266a89f73d6183
Allahveren Mikayilov <mikayilov.allahveren@gmail.com>
2022-12-18 17:34:53 +0400

    add new components

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -3,6 +3,7 @@ import { BsCurrencyDollar } from "react-icons/bs";
 import { GoPrimitiveDot } from "react-icons/go";
 import { IoIosMore } from "react-icons/io";
 import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
+import WelcomeBg from "../data/welcome-bg.svg";
 
 import { Stacked, Pie, Button, LineChart, SparkLine } from "../components";
 import {
@@ -37,7 +38,14 @@ const Ecommerce = () => {
   return (
     <div className="mt-24">
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+        <div
+          className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center"
+          style={{
+            backgroundImage: `url("${WelcomeBg}")`,
+            backgroundSize: "contain",
+            backgroundPosition: "34px 20px",
+          }}
+        >
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
